18c10209e5f902ff67b525fc16ac539e21c29c77
binbandabui <126151527+binbandabui@users.noreply.github.com>
2024-10-29 21:22:14 +0700

    a

--- models/user.js
+++ models/user.js
@@ -56,6 +56,10 @@ const userSchema = mongoose.Schema({
     type: Boolean,
     default: true,
   },
+  cart: {
+    type: Array,
+    default: {},
+  },
   otp: { type: String, default: null },
   otpExpiration: { type: Date, default: null },
 });
